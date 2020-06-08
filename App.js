cc097ab9b3af3475081fb4869d7b6859388e1277
ubermachine <shivamkatoch@outlook.com>
2020-06-08 23:28:21 +0530

    Part 1

--- App.js
+++ App.js
@@ -1,78 +1,10 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-class Count extends React.Component {
-  shouldComponentUpdate(nextState) {
-    return nextState.count % 2;
-  }
-  componentDidUpdate() {
-    console.log(this.props.count);
-  }
+import React from 'react';
+import Main from './components/MainComponent';
 
-  render() {
-    return <Text style={styles.texts}>{this.props.count}</Text>;
-  }
-}
 export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      counter: true,
-    };
-  }
-
-  componentDidMount() {
-    this.intervals = setInterval(this.inc, 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.intervals);
-  }
-
-  inc = () => {
-    console.log("still workjing");
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
-  };
-  toggleCounter = () => {
-    this.setState((prevState) => ({
-      counter: !prevState.counter,
-    }));
-  };
-  //  console.log("donw");
   render() {
-    if (this.state.counter) {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.texts}>Brrrrrrrrrrrrrrr......</Text>
-          <Count count={this.state.count} counter={this.state.counter} />
-          <Button title="toggle" onPress={this.toggleCounter}></Button>
-        </View>
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.texts}>Brrrrrrrrrrrrrrr......</Text>
-
-          <Button title="toggle" onPress={this.toggleCounter}></Button>
-        </View>
-      );
-    }
+    return (
+      <Main />
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  texts: {
-    fontSize: 40,
-    fontWeight: "bold",
-    //fontFamily: "sans",
-    color: "white",
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
