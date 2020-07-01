981b181ff278c638b5ea6d3d9dfa6388e1966ebc
ubermachine <shivamkatoch@outlook.com>
2020-07-01 11:31:01 +0530

    added mail composer to contactComponent

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
 import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
-
+import { Card, Button, Icon } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
+import * as MailComposer from "expo-mail-composer";
 class Contact extends Component {
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ["confusion@food.net"],
+      subject: "Enquiry",
+      body: "To whom it may concern:",
+    });
+  }
   render() {
     return (
       <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -18,6 +25,14 @@ class Contact extends Component {
   Fax: +852 8765 4321
   Email:confusion@food.net`}
             </Text>
+            <Button
+              title="Send Email"
+              buttonStyle={{ backgroundColor: "#512DA8" }}
+              icon={
+                <Icon name="envelope-o" type="font-awesome" color="white" />
+              }
+              onPress={this.sendMail}
+            />
           </Card>
         </View>
       </Animatable.View>
