34f37aea101a388d4c169c5709ca2bfdb7cdf2d0
ubermachine <shivamkatoch@outlook.com>
2020-07-01 20:35:43 +0530

    netifo toast play

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,6 +7,7 @@ import {
   Image,
   StyleSheet,
   SafeAreaView,
+  ToastAndroid,
 } from "react-native";
 import Menu from "./MenuComponent";
 import Dishdetail from "./DishdetailComponent";
@@ -403,16 +404,47 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPromos();
     this.props.fetchLeaders();
+    // NetInfo.fetch().then((state) => {
+    //   ToastAndroid.show(
+    //     "Initial Network Connectivity Type: " +
+    //       state.type +
+    //       ", Connected " +
+    //       state.isConnected,
+    //     ToastAndroid.LONG
+    //   );
+    // });
     const unsubscribe = NetInfo.addEventListener((state) => {
-      console.log("Connection type", state.type);
-      console.log("Is connected?", state.isConnected);
+      this.handleConnectivityChange(state);
     });
   }
 
   componentWillUnmount() {
     unsubscribe();
   }
-
+  handleConnectivityChange = (connectionInfo) => {
+    switch (connectionInfo.type) {
+      case "none":
+        ToastAndroid.show("You are now offline!", ToastAndroid.LONG);
+        break;
+      case "wifi":
+        ToastAndroid.show("You are now connected to WiFi!", ToastAndroid.LONG);
+        break;
+      case "cellular":
+        ToastAndroid.show(
+          "You are now connected to Cellular!",
+          ToastAndroid.LONG
+        );
+        break;
+      case "unknown":
+        ToastAndroid.show(
+          "You now have unknown connection!",
+          ToastAndroid.LONG
+        );
+        break;
+      default:
+        break;
+    }
+  };
   render() {
     return (
       <NavigationContainer>
